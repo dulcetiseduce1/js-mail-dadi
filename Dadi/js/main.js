e1611de4cbf12beb3d40f49cfd89b205ff160583
dulcetiseduce1 <mariusdulce@tuta.io>
2022-05-26 10:14:55 +0200

    dadi

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,57 +1,44 @@
-/* 
-Generare più  numeri random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-C’è forse un array da qualche parte?
-Se dobbiamo confrontare qualcosa che “cosa” ci serve?
- */
+const scoreComputer = []
+const arrayComputer = []
+const scorePlayer = []
+const arrayPlayer = []
+let sommaComputer = 0
+let sommaPlayer = 0
 
-const numeriUtente = [];
-const numeriAi = [];
-const maxNum = 6;
-
-let sommaNumUtente = 0;
-let sommaNumAi = 0;
-
-for (let i = 1; i <= maxNum; i++) {
-  // Generiamo i numeri random per entrambi i giocatori
-  const randomNumberUser = Math.floor(Math.random() * maxNum) + 1;
-  const randomNumberAi = Math.floor(Math.random() * maxNum) + 1;
-
-  // pushiamo nei rispettivi array i numeri generati
-  numeriUtente.push(randomNumberUser);
-  numeriAi.push(randomNumberAi);
-
-  // sommiamo i numeri generati per ogni giocatore
-  sommaNumUtente += randomNumberUser;
-  sommaNumAi += randomNumberAi;
+//ciclo 6 volte
+for (let i = 1; i < 7; i++) {
+    //math da 1 a 6
+    (scoreComputer[i] = (Math.floor(Math.random() * 6) + 1))
+    //push numero in array
+    arrayComputer.push(scoreComputer)
+    console.log("Computer: " + scoreComputer)
+    //aggiungo numero in somma
+    sommaComputer += scoreComputer[i]
 }
+//output test
+console.log("computer: " + sommaComputer)
 
-console.log(sommaNumUtente, sommaNumAi);
-
-if (sommaNumAi > sommaNumUtente) {
-  alert(`L'AI dominerà il mondo! (${sommaNumUtente} - ${sommaNumAi})`);
-} else if (sommaNumUtente > sommaNumAi) {
-  alert(`Questa volta te la sei cavata! (${sommaNumUtente} - ${sommaNumAi})`);
-} else {
-  alert(`Pareggio! (${sommaNumUtente} - ${sommaNumAi})`);
+//ciclo 6 volte
+for (let i = 1; i < 7; i++) {
+  //math da 1 a 6
+    (scorePlayer[i] = (Math.floor(Math.random() * 6) + 1));
+    //push numero in array
+    arrayPlayer.push(scorePlayer)
+    console.log("Player: " + scorePlayer)
+    //aggiungo numero in somma
+    sommaPlayer += scorePlayer[i]
 }
+//output test
+console.log("Player: " + sommaPlayer)
 
-// --------------------------------------------------------------
-// --------------------------------------------------------------
-// --------------------------------------------------------------
-
-const diceContainer = document.querySelector(".dice-container");
-const rollDiceBtn = document.getElementById("roll-dice-btn");
-const diceMap = ["first", "second", "third", "fourth", "fifth", "sixth"];
-
-rollDiceBtn.addEventListener("click", function (e) {
-  const randomNumber = Math.floor(Math.random() * 6) + 1;
-  const diceTarget = diceContainer.querySelector(
-    `.${diceMap[randomNumber - 1]}-face`
-  );
-
-  diceContainer.firstElementChild.style.transform = `translateX(-${
-    diceTarget.offsetLeft - 16
-  }px)`;
-});
+//se la tua somma è maggiore
+if (sommaPlayer > sommaComputer) {
+    console.log("Hai vinto!")
+//se la tua somma è minore 
+} else if (sommaPlayer < sommaComputer) {
+    console.log("Hai perso")
+}
+//se la somma è uguale
+ else {
+    console.log("Pareggio!");
+}
